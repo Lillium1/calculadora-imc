20ea985237a81d02b3215254905401acb1d2c590
kathmarioly <katerinemarioly@gmail.com>
2017-05-08 23:28:18 -0300

    Modificando clone

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-function calculadora (){ 
-	
-}; 
+function Calculadora(){
+  var peso = document.getElementById("peso").value;
+  var altura = document.getElementById("altura").value;
+  var calcular = parsInt((peso)/ (Math.pow(altura, 2)));
+  document.getElementById("calcular").value;
 
-calculadora();
+}
+
+Calculadora()
