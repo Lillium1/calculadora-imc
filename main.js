c83959b4df3f024b93e75cbafd16a2ae07a5d15f
Paulina Gonzalez C <pgc.lacroix@gmail.com>
2017-05-09 00:04:59 -0300

    calculadora funcionando y validada

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 function calculadora (){ 
-	
-}; 
+	//llamar id de html
+	var peso = document.getElementById('peso').value;
+	var altura = document.getElementById('altura').value;
+	var imprimir = document.getElementById("imprimir");
 
-calculadora();
+	// calcular IMC
+	var res = parseInt(peso)*10000/Math.pow(parseInt(altura),2);
+	imprimir.innerHTML = "Tu IMC es: " + res.toFixed(2);
+
+	// validar campo vacio
+	if (peso==undefined || peso=="" || peso.length==0 && altura==undefined || altura=="" || altura.length==0) {
+		alert("Di la verdad Rosa!")
+		imprimir.innerHTML = "";
+	}
+}; 
